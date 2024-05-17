2199d61a89cab87af43d5579804fe50ad4bc8386
bea2071 <96319941+bea2071@users.noreply.github.com>
2024-05-17 16:38:04 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,110 @@
+# # # EXERCÍCIOS: 
+
+# # # 1* 
+# # # Peça para o usuário digitar um número, verifique se um número 
+# # # é positivo, negativo ou zero.
+# #  # [-1,-2,0,1,2,3,5]
+
+# # numero = int(input('igite um número'))
+
+# # if numero > 0:
+# #    print('Positivo')
+# # elif numero < 0:
+# #    print('Negativo')      
+# # else:
+# #    print('É 0')  
+
+
+
+# # # 2*
+
+# # # Peça para o usuário digitar a idade, verifique se uma 
+# # # pessoa pode votar com base na idade.
+
+# # idade  =  int(input('Digite sua idade'))
+# # if idade >=16:
+# #    print('Pode votar')
+# # else:
+# #    print('Não pode votar')
+
+
+
+
+# # 3*
+
+# # Declara uma variável com um número qualquer, determine se 
+# # um número é par ou ímpar.
+
+
+# # n = 11
+# # if n % 2 ==0:
+# #    print('é par')
+# # else: 
+# #    print('é impar')     
+
+
+
+
+# # 4*
+
+# # Usuário vai digitar 3  números, para criar um triângulo, 
+# # verifique se um triângulo é equilátero, isósceles ou escaleno
+
+# # Um triângulo é chamado de equilátero se todos os lados 
+# # possuem a mesma medida. Um triângulo é chamado de isósceles 
+# # se dois lados possuem a mesma medida. Um triângulo é chamado 
+# # de escaleno se todos os lados possuem medidas diferentes.**
+
+# n1 = int(input(''))
+# n2 = int(input(''))
+# n3 = int(input(''))
+
+# if n1 == n2 == n3:
+#    print('Equilatero')
+# elif n1 != n2 != n3:
+#    print('escaleno')
+# else: 
+#    print('Esóceles')         
+
+
+
+
+
+
+# # 5*
+
+# # Determine se um número é múltiplo de 5 e 7.
+
+# n = int(input('Digite'))
+
+# if n % 5 == 0 and n % 7 ==0:
+#    print('multiplo')
+# else: 
+#    print('não é')      
+
+
+
+# # 6*
+
+# # Verifique se um número é positivo e maior que 10
+
+# n = 20
+# if n > 0 and n > 10:
+#    print( 'Verdadeiro' )
+# else:
+#    print('Falso')     
+
+
+
+
+
+# 7*
+
+# Verifique se um número é divisível por 3 ou 5.
+
+
+number  = 15
+if number % 5 ==0 or number % 3 ==0:
+   print('verdadeiro')
+else: 
+   print('Falso')     
